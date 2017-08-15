6c0da4b5c21628fccbe768e9709471b0c24b251f
Kelvin <549652152@qq.com>
2017-08-15 18:25:55 +0800

    Namespace解析ＸＭＬ Demo

--- springmvc/src/test/java/runner/NamespaceTest.java
+++ springmvc/src/test/java/runner/NamespaceTest.java
@@ -6,7 +6,7 @@
  * Author      : Kelvin范显
  * 
  */
-package parseXML;
+package runner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +15,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import parseXML.EnglishHelloWorld;
+import parseXML.FrenchHelloWorld;
+import parseXML.HelloWorldInterface;
 
 import static org.junit.Assert.assertTrue;
 
