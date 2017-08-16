a49570b0749163c38a8e1a51c524478cf0fe53ec
Kelvin <549652152@qq.com>
2017-08-16 17:18:41 +0800

    ServletRequestHandledEventListener代码实现

---
+++ springmvc/src/main/java/com/kelvin/listener/ServletRequestHandledEventListener.java
@@ -0,0 +1,21 @@
+package com.kelvin.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
+
+/**
+ * @author Kelvin范显
+ * @createDate 2017年08月16日
+ */
+@Component
+public class ServletRequestHandledEventListener implements ApplicationListener<ServletRequestHandledEvent> {
+    final static Logger logger = LoggerFactory.getLogger("RequestProcessLog");
+    @Override
+    public void onApplicationEvent(ServletRequestHandledEvent event) {
+        logger.info(event.getDescription());
+        System.out.println(event.getDescription());
+    }
+}
