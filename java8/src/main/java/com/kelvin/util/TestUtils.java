9abd3f9bae52788ee741e7fec4ab5f6ef726d5f7
Kelvin <549652152@qq.com>
2017-08-24 09:56:44 +0800

    Stream.generate(()->("-"))

--- java8/src/main/java/com/kelvin/util/TestUtils.java
+++ java8/src/main/java/com/kelvin/util/TestUtils.java
@@ -1,5 +1,8 @@
 package com.kelvin.util;
 
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 /**
  * @author Kelvin范显
  * @createDate 2017年08月23日
@@ -16,10 +19,11 @@ public class TestUtils {
 
     public void line(Object object){
         int len = object.toString().length()+10;
-        while (len>0){
-            System.out.print("-");
-            len--;
-        }
+        Stream.generate(()->("-")).limit(len).forEach(System.out::print);//java8写法
+//        while (len>0){
+//            System.out.print("-");
+//            len--;
+//        }
         System.out.println();
     }
 }
